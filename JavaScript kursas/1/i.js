43244391e3a093889eee4c3de8a54a6fe85b0972
Edvinas Ramanauskas <edvinyzas.rr@gmail.com>
2021-07-15 21:28:08 +0300

    Js 1 pamoka

---
+++ JavaScript kursas/1/i.js
@@ -0,0 +1,19 @@
+console.log('Labas, Afrika');
+
+
+
+
+let message; // sukuriame kintamaji
+
+console.log(message);
+
+let pukuotaZasis = 33;
+
+
+message = 55; // kintamajam duodam reiksme
+
+message = 77; // kintamajam duodam reiksme
+
+
+console.log(message);
+console.log(pukuotaZasis);
